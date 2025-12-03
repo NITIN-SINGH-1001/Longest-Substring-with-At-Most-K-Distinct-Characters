05df6935aeca5cb65c7956bc091583ab19f3d3a2
Nitin Singh <nitin.pro.004@gmail.com>
2025-12-03 11:12:32 +0530

    1 commit

---
+++ SlidingWindow/q1.cpp
@@ -0,0 +1,32 @@
+// Longest Substring with At Most K Distinct Characters
+// Solved on "Code 360"
+
+#include<bits/stdc++.h>
+
+int kDistinctChars(int k, string &str)
+{
+    // Write your code here
+    unordered_map<int,int>mp;
+    int ans=0;
+    
+    int l=0,r=0,n=str.size();
+    while(r < n)
+    {
+        mp[str[r]]++;
+
+        if(mp.size() > k)
+        {
+            mp[str[l]]--;
+            if(mp[str[l]]==0)
+            {
+                mp.erase(str[l]);
+            }
+            l++;
+        }
+        ans=max(ans,(r-l+1));
+        r++;
+    }
+    return ans;
+}
+
+
---
+++ SlidingWindow/q2.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cout<<"Hello World"<<endl;
+    cout<<"This is just a random file i was just checking something"<<endl;
+
+   return 0;
+}
